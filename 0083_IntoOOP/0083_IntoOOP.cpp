78b7ccd38d6de77845e85d2b53a7e59b93a7f77d
feraistanti <fistanti551@gmail.com>
2024-04-22 16:21:32 +0700

    menuliskan include iostream

--- 0083_IntoOOP/0083_IntoOOP.cpp
+++ 0083_IntoOOP/0083_IntoOOP.cpp
@@ -1,14 +1,2 @@
 #include <iostream>
-using namespace std;
-
-class Mahasiswa {
-public:
-	string nama;
-	int umur;
-	string jurusan;
-	void output() {
-		cout << "Nama: " << nama << endl;
-		cout << "Umur: " << umur << endl;
-		cout << " Jurusan " << jurusan << endl;
-	}
 
