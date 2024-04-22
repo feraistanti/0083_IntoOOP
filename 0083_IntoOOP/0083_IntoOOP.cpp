a4cec0d15ed5e22b3e1074003d03ae2794fda27f
feraistanti <fistanti551@gmail.com>
2024-04-22 16:58:53 +0700

    menuliskan include iostream dan using namespace

--- 0083_IntoOOP/0083_IntoOOP.cpp
+++ 0083_IntoOOP/0083_IntoOOP.cpp
@@ -6,15 +6,15 @@ public:
 	string nama;
 	int umur;
 	string jurusan;
-}
+
 	void output() {
-		void output() {
+
 			cout << "Nama: " << nama << endl;
 			cout << "Umur: " << umur << endl;
 			cout << " Jurusan " << jurusan << endl;
 		}
+};
 
-	};
 	class Matakuliah {
 	private:
 		string kodeMK;
